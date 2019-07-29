79ebea4dc4158948037ff2be7b97a0ff81f653f9
Piotr Wyszkowski <piotr.wyszkowski@outlook.com>
2019-07-29 18:46:39 +0200

    Add comment for fizzbuzz check method

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,10 +1,17 @@
 using System;
 
-namespace FizzBuzz 
+namespace FizzBuzz
 {
     public class FizzBuzz
     {
-        public string Check(int number){
+        /// <param name="number">input number</param>
+        /// <returns>
+        /// fizz - for number divided by 3
+        /// buzz - for number divided by 5
+        /// fizzbuzz - for number divided by 3 and 5
+        /// </returns>
+        public string Check(int number)
+        {
             throw new NotImplementedException();
         }
     }
