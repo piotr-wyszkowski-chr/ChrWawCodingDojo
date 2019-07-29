e313e78d40679609b730e567b348fe774a333563
Piotr Wyszkowski <piotr.wyszkowski@outlook.com>
2019-07-29 19:14:58 +0200

    Add WordWrap kata

---
+++ WordWrap/WordWrapTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace WordWrap
+{
+    public class WordWrapTest
+    {
+        private WordWrap _sut = new WordWrap();
+
+        [Fact]
+        public void Check_State_Result()
+        {
+
+        }
+    }
+}
