69c49622f2f680f2653b926eedf53fae71ee270f
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2019-07-31 16:39:55 +0200

    Add more detailed description

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -9,6 +9,7 @@ namespace FizzBuzz
         /// fizz - for number divisible by 3
         /// buzz - for number divisible by 5
         /// fizzbuzz - for number divisible by 3 and 5
+        /// inputNumber - for number not divisible by 3 and 5
         /// </returns>
         public string Check(int number)
         {
