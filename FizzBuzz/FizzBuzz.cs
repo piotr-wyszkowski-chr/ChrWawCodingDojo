ddc5743befb44befe4451a36ca934d2b5c0f7614
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2019-08-01 09:13:04 +0200

    - code after codingDojo #1 31.07.2019

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -13,7 +13,26 @@ namespace FizzBuzz
         /// </returns>
         public string Check(int number)
         {
-            throw new NotImplementedException();
+            string resultValue = string.Empty;
+            bool isDivBy3 = number % 3 == 0;
+            bool isDivBy5 = number % 5 == 0;
+
+
+            if(isDivBy3 && isDivBy5){
+                resultValue = "FIZZBUZZ";
+            }
+            else if(isDivBy3){
+                resultValue = "FIZZ";
+            }
+            else if(isDivBy5){
+                resultValue = "BUZZ";
+            }
+            else
+            {
+                resultValue = number.ToString();
+            }
+
+            return resultValue;
         }
     }
 }
--- FizzBuzz/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzzTest.cs
@@ -7,9 +7,43 @@ namespace FizzBuzz
         private FizzBuzz _sut = new FizzBuzz();
 
         [Fact]
-        public void Check_State_Result()
+        public void tc1()
         {
+            int number = 3;
+            string returnValue = _sut.Check(number);
+            Assert.NotNull(returnValue);
+            Assert.NotEmpty(returnValue);
+            Assert.Equal("FIZZ", returnValue.ToUpper());
+        }
 
+        [Fact]
+        public void tc2()
+        {
+            int number = 5;
+            string returnValue = _sut.Check(number);
+            Assert.NotNull(returnValue);
+            Assert.NotEmpty(returnValue);
+            Assert.Equal("BUZZ", returnValue.ToUpper());
+        }
+
+                [Fact]
+        public void tc3()
+        {
+            int number = 15;
+            string returnValue = _sut.Check(number);
+            Assert.NotNull(returnValue);
+            Assert.NotEmpty(returnValue);
+            Assert.Equal("FIZZBUZZ", returnValue.ToUpper());
+        }
+
+                        [Fact]
+        public void tc4()
+        {
+            int number = 4;
+            string returnValue = _sut.Check(number);
+            Assert.NotNull(returnValue);
+            Assert.NotEmpty(returnValue);
+            Assert.Equal("4", returnValue.ToUpper());
         }
     }
 }
