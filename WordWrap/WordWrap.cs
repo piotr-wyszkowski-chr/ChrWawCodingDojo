876b2d130863d461b1055779dbf6230401d83114
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2019-08-01 09:55:36 +0200

    - Not finished WordWrap kata after CodingDojo #1

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -5,14 +5,58 @@ namespace WordWrap
     public class WordWrap
     {
         /*
-         * You write a class called Wrapper, that has a single function named wrap that takes two arguments, a string, and a column number.
-         * The function returns the string, but with line breaks inserted at just the right places to make sure that no line is longer than the column number.
-         * You try to break lines at word boundaries.
-         * Like a word processor, break the line by replacing the last space in a line with a newline.
+         Your job is to write a function that takes a string and a maximum number of characters per line and then inserts line breaks as necessary so that no line in the resulting string is longer than the specified limit.
+
+        If possible, line breaks should not split words. However, if a single word is longer than the limit, it obviously has to be split. In this case, the line break should be placed after the first part of the word (see examples below).
+
+        Really long words may need to be split multiple times.
+
+        #Input
+        A word consists of one or more letters.
+
+        Input text will be the empty string or a string consisting of one or more words separated by single spaces. It will not contain any punctiation or other special characters.
+
+        The limit will always be an integer greater or equal to one.
+
+        #Examples
+
+        Note: Line breaks in the results have been replaced with two dashes to improve readability.
+
+        ("test", 7) -> "test"
+        ("hello world", 7) -> "hello--world"
+        ("a lot of words for a single line", 10) -> "a lot of--words for--a single--line"
+        ("this is a test", 4) -> "this--is a--test"
+        ("a longword", 6) -> "a long--word"
+        ("areallylongword", 6) -> "areall--ylongw--ord"
+        Note: Sometimes spaces are hard to see in the test results window.
          */
-        public string Wrap(string input, int columnNumber)
+        public string Wrap(string input, int lineCharactersCount)
         {
-            throw new NotImplementedException();
+            var words = input.Split(" ");
+            string result = string.Empty;
+            if (input.Length < lineCharactersCount)
+            {
+                return input;
+            }
+
+            var index = 1;
+            result = words[0];
+            string line = string.Empty;
+
+            while (index < words.Length)
+            {
+                string word = words[index];
+
+                while (line.Length < lineCharactersCount)
+                {
+                    line += word + ' ';
+                }
+                line += "--";
+                index++;
+            }
+
+
+            return result;
         }
     }
 }
--- WordWrap/WordWrapTest.cs
+++ WordWrap/WordWrapTest.cs
@@ -7,9 +7,42 @@ namespace WordWrap
         private WordWrap _sut = new WordWrap();
 
         [Fact]
-        public void Check_State_Result()
+        public void tc1()
         {
+            string test = "test";
+            int colLength = 7;
 
+            string res = _sut.Wrap(test, colLength);
+
+            Assert.NotNull(res);
+            Assert.NotEmpty(res);
+            Assert.Equal("test", res);
+        }
+
+        [Fact]
+        public void tc2()
+        {
+            string test = "hello world";
+            int colLength = 7;
+
+            string res = _sut.Wrap(test, colLength);
+
+            Assert.NotNull(res);
+            Assert.NotEmpty(res);
+            Assert.Equal("hello--world", res);
+        }
+
+        [Fact]
+        public void tc3()
+        {
+            string test = "a lot of words for a single line";
+            int colLength = 10;
+
+            string res = _sut.Wrap(test, colLength);
+
+            Assert.NotNull(res);
+            Assert.NotEmpty(res);
+            Assert.Equal("a lot of--words for--a single--line", res);
         }
     }
 }
