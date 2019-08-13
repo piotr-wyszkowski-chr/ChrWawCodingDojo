f3a473814707c143545c3e7ff62d989dacf2d3b4
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2019-08-13 14:35:53 +0200

    Add empty RomanNumbers Kata

---
+++ RomanNumbers/RomanNumbers.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RomanNumbers
+{
+    public class RomanNumbers
+    {
+        /// <param name="romanNumber">number in Roman format e.g. IV, V, X etc.</param>
+        /// <returns>Arabic number</returns>
+        public int Exchange(string romanNumber)
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
---
+++ RomanNumbers/RomanNumbersTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace RomanNumbers
+{
+    public class RomanNumbersTest
+    {
+        private RomanNumbers _sut = new RomanNumbers();
+
+        [Fact]
+        public void Exchange_State_Result()
+        {
+        }
+    }
+}
