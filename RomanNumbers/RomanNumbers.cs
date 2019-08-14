80fa35d96210bf7fc6f1e5355d8556fe31e6cbdc
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2019-08-14 08:58:40 +0200

    CodingDojo #2

--- RomanNumbers/RomanNumbers.cs
+++ RomanNumbers/RomanNumbers.cs
@@ -1,14 +1,62 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace RomanNumbers
 {
     public class RomanNumbers
     {
+        private readonly Dictionary<string, int> Symbols;
+
+        public RomanNumbers()
+        {
+            Symbols = new Dictionary<string, int>(){
+                {"I", 1},
+                {"V", 5},
+                {"X", 10},
+                {"L", 50},
+                {"C", 100},
+                {"D", 500},
+                {"M", 1000},
+                
+            };
+        }
+
         /// <param name="romanNumber">number in Roman format e.g. IV, V, X etc.</param>
         /// <returns>Arabic number</returns>
         public int Exchange(string romanNumber)
         {
-           throw new NotImplementedException();
+            var charArray = romanNumber.Reverse().ToArray();
+                    var char0 = charArray[0];
+                    int val0 = Symbols[char0.ToString()];
+
+            int result = val0;
+
+            for (int i = 0; i < charArray.Length-1; i++)
+            {
+                if (i <= charArray.Length)
+                {
+                    var firstChar = charArray[i];
+                    var secondChar = charArray[i + 1];
+                    
+                    int firstVal = Symbols[firstChar.ToString()];
+                    int secondVal = Symbols[secondChar.ToString()];
+
+                    if(firstVal <= secondVal)
+                    {
+                        result += secondVal;
+                    }
+                    else
+                    {
+                        result -= secondVal;
+                    }
+                }
+                
+            }
+
+
+           return result;
+
+
         }
     }
 }
--- RomanNumbers/RomanNumbersTest.cs
+++ RomanNumbers/RomanNumbersTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace RomanNumbers
 {
@@ -6,9 +7,34 @@ namespace RomanNumbers
     {
         private RomanNumbers _sut = new RomanNumbers();
 
-        [Fact]
-        public void Exchange_State_Result()
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("II", 2)]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("V", 5)]
+        [InlineData("VI", 6)]
+        [InlineData("VII", 7)]
+        [InlineData("VIII", 8)]
+        [InlineData("X", 10)]
+        [InlineData("L", 50)]
+        [InlineData("C", 100)]
+        [InlineData("D", 500)]
+        [InlineData("M", 1000)]
+        [InlineData("CM",900)]
+        [InlineData("MCDXLIV",1444)]
+        [InlineData("MMMCDXLIV",3444)]
+        public void Exchange_CorrectRomanNumber_CorrectNumber(string input, int expected)
         {
+            var result = _sut.Exchange(input);
+            Assert.Equal(expected, result);
         }
+
+        public void Exchange_IncorrectRomanNumber_ThrowException(string input)
+        {
+            var result = _sut.Exchange(input);
+            Assert.Throws<ArgumentException>(() => _sut.Exchange(input));
+        }
+
     }
 }
