50e7f2b610bfdeed87e2d388397c64c242306cc4
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2019-08-28 15:57:59 +0200

    Add empty PolidromeChecker project

---
+++ PolidromeChecker/PolidromeChecker.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PolidromeChecker
+{
+    public class PolidromeChecker
+    {
+        /*
+Modify Check method to return a boolean (true/false) to indicate if the inputed string is a palindrome.
+
+Example:
+
+"ABBA" is a palindrome
+"ABAB" is not a palindrome
+         */
+        public bool Check(bool input)
+        {
+            
+        }
+    }
+}
---
+++ PolidromeChecker/PolidromeCheckerTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace PolidromeChecker
+{
+    public class PolidromeCheckerTest
+    {
+        private PolidromeChecker _sut = new PolidromeChecker();
+
+        [Fact]
+        public void Method_State_Result()
+        {
+         
+        }
+    }
+}
