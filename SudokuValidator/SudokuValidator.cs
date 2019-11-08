7c3dc5d51cddbb54992fa914ae601f19d39543fb
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2019-11-08 13:41:10 +0100

    CHR Coding dojo #7

--- SudokuValidator/SudokuValidator.cs
+++ SudokuValidator/SudokuValidator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SudokuValidator
 {
@@ -6,7 +8,78 @@ namespace SudokuValidator
     {
         public bool Validate(int[][] input)
         {
-            throw new NotImplementedException();
+            return GetRows(input).All(IsValid) && GetColumns(input).All(IsValid) && GetSectors(input).All(IsValid);
+        }
+
+        public bool IsValid(int[] input)
+        {
+            if (input.Length != 9)
+            {
+                return false;
+            }
+
+            if (!input.All(x => x >= 1 && x <= 9))
+            {
+                return false;
+            }
+
+            if (input.Distinct().Count() != 9)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        public List<int[]> GetRows(int[][] input)
+        {
+            return input.ToList();
+        }
+
+        public List<int[]> GetColumns(int[][] input)
+        {
+            var columns = new List<int[]>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var revertedColumn = new int[input.Length];
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    revertedColumn[j] = input[j][i];
+                }
+                columns.Add(revertedColumn);
+            }
+
+            return columns;
+        }
+
+        public List<int[]> GetSectors(int[][] input)
+        {
+            var sectors = new List<int[]>();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sectors.Add(GetSector(input, i, j));
+                }
+            }
+
+            return sectors;
+        }
+
+        private int[] GetSector(int[][] input, int sectorX, int sectorY)
+        {
+            var sector = new int[9];
+            int sectorIndex = 0;
+            for (int i = sectorX * 3; i < sectorX * 3 + 3; i++)
+            {
+                for (int j = sectorY * 3; j < sectorY * 3 + 3; j++)
+                {
+                    sector[sectorIndex++] = input[i][j];
+                }
+            }
+
+            return sector;
         }
     }
 }
--- SudokuValidator/SudokuValidatorTests.cs
+++ SudokuValidator/SudokuValidatorTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,6 +9,32 @@ namespace SudokuValidator
     public class SudokuValidatorTests
     {
         private SudokuValidator _sut;
+        private int[][] _incorrect = new[]
+        {
+            new[] {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            new[] {6, 7, 2, 1, 9, 0, 3, 4, 8},
+            new[] {1, 0, 0, 3, 4, 2, 5, 6, 0},
+            new[] {8, 5, 9, 7, 6, 1, 0, 2, 0},
+            new[] {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            new[] {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            new[] {9, 0, 1, 5, 3, 7, 2, 1, 4},
+            new[] {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            new[] { 3, 0, 0, 4, 8, 1, 1, 7, 9 }
+        };
+
+        private int[][] _correct = new[]
+        {
+            new[] {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            new[] {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            new[] {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            new[] {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            new[] {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            new[] {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            new[] {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            new[] {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            new[] {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+
 
         [TestInitialize]
         public void Initialize()
@@ -17,7 +45,76 @@ namespace SudokuValidator
         [TestMethod]
         public void Validate_CorrectSudoku_true()
         {
-            var input = new[]
+
+            _sut.Validate(_correct).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Validate_IncorrectSudoku_false()
+        {
+
+            _sut.Validate(_incorrect).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsValid_LengthLessThan9_false()
+        {
+            var input = new int[] { 1, 2 };
+            _sut.IsValid(input).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsValid_LengthGreaterThan9_false()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            _sut.IsValid(input).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsValid_LengthEqual9_true()
+        {
+            var input = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            _sut.IsValid(input).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValid_AreElementsInRange_true()
+        {
+            var input = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            _sut.IsValid(input).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValid_AreElementsNotInRange_false()
+        {
+            var input = new[] { 1, -1, 66, 3, 4, 5, 6, 7, 8 };
+            _sut.IsValid(input).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsValid_AreElementsUnique_true()
+        {
+            var input = new[] { 1, 2, 3, 4, 6, 7, 8, 9, 5 };
+            _sut.IsValid(input).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValid_AreElementsUnique_false()
+        {
+            var input = new[] { 1, 1, 3, 4, 6, 7, 8, 9, 5 };
+            _sut.IsValid(input).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void GetRows_CorrectNumberOfRows()
+        {
+            _sut.GetRows(_correct).Should().HaveCount(9);
+        }
+
+        [TestMethod]
+        public void GetRows_CorrectRowsData()
+        {
+            var expectedResult = new List<int[]>
             {
                 new[] {5, 3, 4, 6, 7, 8, 9, 1, 2},
                 new[] {6, 7, 2, 1, 9, 5, 3, 4, 8},
@@ -29,8 +126,74 @@ namespace SudokuValidator
                 new[] {2, 8, 7, 4, 1, 9, 6, 3, 5},
                 new[] {3, 4, 5, 2, 8, 6, 1, 7, 9}
             };
-
-            _sut.Validate(input).Should().BeTrue();
+            var resultList = _sut.GetRows(_correct);
+            resultList.Should().BeEquivalentTo(expectedResult);
         }
+
+        [TestMethod]
+        public void GetColumns_CorrectNumberOfColumns()
+        {
+            _sut.GetColumns(_correct).Should().HaveCount(9);
+        }
+
+        [TestMethod]
+        public void GetColumns_CorrectColumnsData()
+        {
+            var input = new int[][]
+            {
+                new[] {5, 3, 4},
+                new[] {6, 7, 2},
+                new[] {1, 9, 8}
+            };
+
+            var expectedResult = new List<int[]>
+            {
+                new[] {5, 6, 1},
+                new[] {3, 7, 9},
+                new[] {4, 2, 8}
+            };
+            var resultList = _sut.GetColumns(input);
+            resultList.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [TestMethod]
+        public void GetSectors_CorrectNumberOfSectors()
+        {
+            _sut.GetSectors(_correct).Should().HaveCount(9);
+        }
+
+        [TestMethod]
+        public void GetSectors_CorrectSectorsData()
+        {
+            var input = new int[][]
+            {
+                new []{1,2,3,4,5,6,7,8,9},
+                new []{1,2,3,4,5,6,7,8,9},
+                new []{1,2,3,4,5,6,7,8,9},
+                new []{1,2,3,4,5,6,7,8,9},
+                new []{1,2,3,4,5,6,7,8,9},
+                new []{1,2,3,4,5,6,7,8,9},
+                new []{1,2,3,4,5,6,7,8,9},
+                new []{1,2,3,4,5,6,7,8,9},
+                new []{1,2,3,4,5,6,7,8,9},
+            };
+
+            var expectedResult = new List<int[]>
+            {
+                new[] {1,2,3,1,2,3,1,2,3},
+                new[] {1,2,3,1,2,3,1,2,3},
+                new[] {1,2,3,1,2,3,1,2,3},
+                new[] {4,5,6, 4,5,6,4,5,6},
+                new[] {4,5,6, 4,5,6,4,5,6},
+                new[] {4,5,6, 4,5,6,4,5,6},
+                new[] {7,8,9,7,8,9,7,8,9},
+                new[] {7,8,9,7,8,9,7,8,9},
+                new[] {7,8,9,7,8, 9, 7,8,9}
+            };
+            var resultList = _sut.GetSectors(input);
+            resultList.Should().BeEquivalentTo(expectedResult);
+        }
+
+
     }
 }
