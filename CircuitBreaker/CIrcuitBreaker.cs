9b7e39caefc8df6f6d020562b51314403ff616b7
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2020-01-14 16:54:54 +0100

    CHR coding dojo #10

---
+++ CircuitBreaker/CIrcuitBreaker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CircuitBreaker
+{
+    public class CIrcuitBreaker
+    {
+        public CIrcuitBreaker(Action callback)
+        {
+            openCallback = callback;
+        }
+
+        private const int Threshold = 3;
+        private const int ThresholdTimeout = 3000;
+        private int failureCount = 0;
+        private Action openCallback;
+
+        public void Execute(Action action)
+        {
+            try
+            {
+                if (State == CircuitBreakerState.Open)
+                {
+                    openCallback.Invoke();
+                }
+                else
+                {
+
+
+                    action.Invoke();
+                    State = CircuitBreakerState.Closed;
+                    failureCount = 0;
+                }
+            }
+            catch (Exception e)
+            {
+                failureCount++;
+                if (failureCount >= Threshold && State != CircuitBreakerState.Open)
+                {
+                    State = CircuitBreakerState.Open;
+                    Task.Run(async () =>
+                    {
+                        await Task.Delay(ThresholdTimeout);
+                        State = CircuitBreakerState.HalfOpen;
+                    });
+                }
+            }
+        }
+
+        public CircuitBreakerState State { get; private set; } = CircuitBreakerState.Closed;
+    }
+
+    public enum CircuitBreakerState
+    {
+        Closed = 1,
+        Open = 2,
+        HalfOpen = 3
+    }
+}
---
+++ CircuitBreaker/CircuitBreakerTests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CircuitBreaker
+{
+    [TestClass]
+    public class CircuitBreakerTests
+    {
+        private CIrcuitBreaker _sut;
+        private bool callbackExecuted = false;
+
+        [TestInitialize]
+        public void Init()
+        {
+            callbackExecuted = false;
+            _sut = new CIrcuitBreaker(Callback);
+        }
+
+        [TestMethod]
+        public void Execute_Success_StateClosed()
+        {
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+            _sut.Execute(Success);
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+        }
+
+        [TestMethod]
+        public void Execute_Failure_Under_Threshold_StateClosed()
+        {
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+        }
+
+        [TestMethod]
+        public void Execute_Failure_Equal_Threshold_StateOpen()
+        {
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.State.Should().Be(CircuitBreakerState.Open);
+        }
+
+        [TestMethod]
+        public void Execute_Failure_Before_Timeout()
+        {
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.State.Should().Be(CircuitBreakerState.Open);
+            _sut.Execute(Success);
+            callbackExecuted.Should().Be(true);
+        }
+
+        [TestMethod]
+        public async Task Execute_Failure_After_Timeout()
+        {
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.State.Should().Be(CircuitBreakerState.Open);
+
+            await Task.Delay(4000);
+
+
+            _sut.State.Should().Be(CircuitBreakerState.HalfOpen);
+
+
+            callbackExecuted.Should().Be(false);
+        }
+
+        [TestMethod]
+        public async Task Execute_HalfOpen_ExecuteFailure_Open()
+        {
+           
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.State.Should().Be(CircuitBreakerState.Open);
+
+            await Task.Delay(4000);
+
+
+            _sut.State.Should().Be(CircuitBreakerState.HalfOpen);
+
+            _sut.Execute(Failure);
+
+            _sut.State.Should().Be(CircuitBreakerState.Open);
+
+            callbackExecuted.Should().Be(false);
+        }
+
+        [TestMethod]
+        public async Task Execute_HalfOpen_ExecuteSuccess_Closed()
+        {
+
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.Execute(Failure);
+            _sut.State.Should().Be(CircuitBreakerState.Open);
+
+            await Task.Delay(4000);
+
+
+            _sut.State.Should().Be(CircuitBreakerState.HalfOpen);
+
+            _sut.Execute(Success);
+
+            _sut.State.Should().Be(CircuitBreakerState.Closed);
+
+            callbackExecuted.Should().Be(false);
+        }
+
+        private void Success()
+        {
+        }
+
+        private void Failure()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Callback()
+        {
+            callbackExecuted = true;
+        }
+
+    }
+}
