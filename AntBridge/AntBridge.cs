360f41d030700bcee4bf61a96efcbaca2e3a9f90
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2020-01-31 10:23:45 +0100

    CHR coding dojo #12 Preparation

---
+++ AntBridge/AntBridge.cs
@@ -0,0 +1,11 @@
+﻿namespace AntBridge
+{
+    public class AntBridge
+    {
+        public string Bridge(string ants, string terrain)
+        {
+            // Your code here!
+            return ants;
+        }
+    }
+}
---
+++ AntBridge/AntBridgeTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AntBridge
+{
+    [TestClass]
+    public class AntBridgeTests
+    {
+        private AntBridge _sut = new AntBridge();
+
+        [TestMethod]
+        public void Example()
+        {
+            _sut.Bridge("GFEDCBA", "------------...-----------").Should().Be("EDCBAGF");
+        }
+
+        [TestMethod]
+        public void NoGaps()
+        {
+            _sut.Bridge("GFEDCBA", "-----------------------").Should().Be("GFEDCBA");
+        }
+
+
+    }
+}
