3a972d972bce09bf5387ac218f4c365b8e0646ef
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2020-02-14 13:47:02 +0100

    CHR coding dojo #13

--- Heart/Heart.cs
+++ Heart/Heart.cs
@@ -1,12 +1,86 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
 namespace Heart
 {
     public class Heart
     {
+        private const char TopLeft = '◢';
+        private const char TopRight= '◣';
+        private const char Middle = '█';
+        private const char BottomLeft = '◥';
+        private const char BottomRight = '◤';
+
         public string Draw(int n)
         {
             throw new NotImplementedException();
         }
+
+        public string DrawTop(int width)
+        {
+            int squares = width / 2 - 2;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(TopLeft);
+            for (int i = 0; i < squares; i++)
+            {
+                sb.Append(Middle);
+            }
+            sb.Append(TopRight);
+            return sb.ToString() + sb.ToString();
+        }
+        public string DrawMiddle(int width)
+        {
+            int lineCnt = width / 6;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < lineCnt; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    sb.Append(Middle);
+                }
+                sb.Append(Environment.NewLine);
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string DrawBottom(int width)
+        {
+            int height = width / 2;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < height; i++)
+            {
+                StringBuilder line = new StringBuilder();
+
+               
+
+                for (int j = 0; j < i; j++)
+                {
+                    sb.Append(" ");
+                }
+
+                sb.Append(BottomLeft);
+
+                for (int j = 0; j < i; j++)
+                {
+                    sb.Append(" ");
+                }
+
+                sb.Append(line);
+
+                sb.Append(Environment.NewLine);
+
+                
+                
+            }
+
+            return sb.ToString();
+        }
+
+
     }
 }
--- Heart/HeartTests.cs
+++ Heart/HeartTests.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Heart
 {
     [TestClass]
     public class UnitTest1
     {
+        private Heart _sut = new Heart();
+
         private string heart6 =
             "◢█◣◢█◣" +
             "██████" +
@@ -58,8 +63,61 @@ namespace Heart
             "　　　　　　　　◥◤　　　　　　　　";
 
         [TestMethod]
-        public void TestMethod1()
+        [DataRow(6, "◢█◣◢█◣")]
+        [DataRow(8, "◢██◣◢██◣")]
+        [DataRow(10, "◢███◣◢███◣")]
+        [DataRow(12, "◢████◣◢████◣")]
+        [DataRow(18, "◢███████◣◢███████◣")]
+        public void DrawTop(int width, string expectedResult)
         {
+            _sut.DrawTop(width).Should().Be(expectedResult);
+
+        }
+        [TestMethod]
+        [DataRow(6, 1)]
+        [DataRow(8, 1)]
+        [DataRow(10, 1)]
+        [DataRow(12, 2)]
+        [DataRow(18, 3)]
+        public void DrawMiddle(int width, int expectedLineCount)
+        {
+            var result = _sut.DrawMiddle(width)
+                .Split(Environment.NewLine);
+            result.Should().HaveCount(expectedLineCount);
+            result.ToList().ForEach(x=>x.Should().HaveLength((width)));
+        }
+
+        [TestMethod]
+        [DataRow(6, @"◥████◤
+　◥██◤　
+　　◥◤　　")]
+        [DataRow(8, @"　◥██████◤　
+　　◥████◤　　
+　　　◥██◤　　　
+　　　　◥◤　　　　")]
+        [DataRow(10, @"◥████████◤
+　◥██████◤　
+　　◥████◤　　
+　　　◥██◤　　　
+　　　　◥◤　　　　")]
+        [DataRow(12, @"◥██████████◤
+　◥████████◤　
+　　◥██████◤　　
+　　　◥████◤　　　
+　　　　◥██◤　　　　
+　　　　　◥◤　　　　　")]
+        [DataRow(18, @"◥████████████████◤
+　◥██████████████◤　
+　　◥████████████◤　　
+　　　◥██████████◤　　　
+　　　　◥████████◤　　　　
+　　　　　◥██████◤　　　　　
+　　　　　　◥████◤　　　　　　
+　　　　　　　◥██◤　　　　　　　
+　　　　　　　　◥◤　　　　　　　　")]
+        public void DrawBottom(int width, string expectedResult)
+        {
+            _sut.DrawBottom(width).Should().Be(expectedResult);
         }
     }
 }
