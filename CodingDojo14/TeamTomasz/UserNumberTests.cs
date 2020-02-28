92f708be3a05e8749e60b959ead68a407525f576
Tomasz Legowski <LEGOTOM@chrobinson.com>
2020-02-28 10:52:57 +0100

    add test cases

--- CodingDojo14/TeamTomasz/UserNumberTests.cs
+++ CodingDojo14/TeamTomasz/UserNumberTests.cs
@@ -7,9 +7,18 @@ namespace TeamTomasz
     {
         private readonly UserNumber _sut = new UserNumber();
 
+        [DataRow(1, 1)]
+        [DataRow(5, 4)]
+        [DataRow(10, 8)]
+        [DataRow(12, 10)]
+        [DataRow(25, 20)]
+        [DataRow(875, 500)]
+        [DataRow(1860, 1000)]
+        [DataRow(303250, 100000)]
         [TestMethod]
-        public void FirstTest()
+        public void FirstTest(int result, int usersCount)
         {
+            Assert.AreEqual(result, _sut.GetNumber(usersCount));
         }
     }
 }
