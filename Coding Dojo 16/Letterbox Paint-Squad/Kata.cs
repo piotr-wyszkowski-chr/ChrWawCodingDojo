bbe5e5adef6fe183cd4bc3fa87362fe848a4e9f7
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2021-08-06 13:28:04 +0200

    Solution of LetterBox Paint Squad exercise after Coding Dojo #16

--- Coding Dojo 16/Letterbox Paint-Squad/Kata.cs
+++ Coding Dojo 16/Letterbox Paint-Squad/Kata.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Letterbox_Paint_Squad
 {
@@ -6,8 +8,40 @@ namespace Letterbox_Paint_Squad
     {
         public static IEnumerable<int> PaintLetterBoxes(int start, int end)
         {
-            // Your code here
-            return new int[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            if (start <= 0 || end < start)
+            {
+                throw new ArgumentException();
+            }
+
+            var result = new[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            var res = Enumerable
+                .Range(start, end - start + 1)
+                .SelectMany(GetDigits)
+                .GroupBy(digits => digits);
+
+            foreach (var digitGrouping in res)
+            {
+                result[digitGrouping.Key] = digitGrouping.Count();
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<int> GetDigits(int value)
+        {
+            if (value == 0)
+            {
+                yield return 0;
+                yield break;
+            }
+
+            while (value > 0) 
+            {
+                var digit = value % 10;
+                yield return digit;
+                value = value / 10;
+            }
         }
     }
 }
