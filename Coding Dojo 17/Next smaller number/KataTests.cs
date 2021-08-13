9dce6134750d80a8c02da73c776a8fb66d2d32ce
Piotr Wyszkowski <WYSZPIO@chrobinson.com>
2021-08-13 13:36:09 +0200

    Solution of Next smaller number topic during Coding Dojo #17

--- Coding Dojo 17/Next smaller number/KataTests.cs
+++ Coding Dojo 17/Next smaller number/KataTests.cs
@@ -1,12 +1,25 @@
-﻿using Xunit;
+﻿using FluentAssertions;
+using Xunit;
 
 namespace Next_smaller_number
 {
     public class KataTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(9, -1)]
+        [InlineData(111, -1)]
+        [InlineData(135, -1)]
+        [InlineData(1027, -1)]
+        [InlineData(1234560, 1234506)]
+        [InlineData(123456, -1)]
+        [InlineData(long.MaxValue, 9223372036854775780)]
+        [InlineData(20, -1)]
+        [InlineData(-1, -1)]
+        [InlineData(71, 17)]
+        [InlineData(531, 513)]
+        public void NextSmallerNumber(long input, long expected)
         {
+            Kata.NextSmallerNumber(input).Should().Be(expected);
         }
     }
 }
