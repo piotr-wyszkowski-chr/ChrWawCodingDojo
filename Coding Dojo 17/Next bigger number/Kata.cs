4d77a0ea6aef4fee75da77f5f68ff64f7833ca6f
Michal Lotocki <LOTOMIC@chrobinson.com>
2021-08-16 09:13:01 +0200

    wykonanie

---
+++ Coding Dojo 17/Next bigger number/Kata.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Next_bigger_number
+{
+    public class Kata
+    {
+        public static long NextBigger(long n)
+        {
+            if (n < 0)
+            {
+                return -1;
+            }
+
+            var number = n.ToString().ToArray();
+            var sortedNumber = string.Concat(number.OrderByDescending(x => x));
+            if (sortedNumber == string.Concat(number))
+            {
+                return -1;
+            }
+
+            int foundIndex = -1;
+            for (int i = number.Length - 1; i > 0; i--)
+            {
+                var memoryNum = number[i];
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (number[j] < memoryNum)
+                    {
+                        var newNumber = number[j];
+                        number[j] = number[i];
+                        number[i] = newNumber;
+                        foundIndex = j;
+                        break;
+                    }
+                }
+                if (foundIndex > -1)
+                {
+                    break;
+                }
+            }
+
+            var left = number[..(foundIndex + 1)];
+            var right = number[(foundIndex + 1)..];
+
+            Array.Sort(right);
+
+            return long.Parse(string.Concat(left.Concat(right)));
+        }
+    }
+}
---
+++ Coding Dojo 17/Next bigger number/KataTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Next_bigger_number
+{
+    public class KataTests
+    {        
+        [Theory]
+        [InlineData(-1, -1)]
+        [InlineData(1, -1)]
+        [InlineData(12, 21)]
+        [InlineData(513, 531)]
+        [InlineData(2017, 2071)]
+        [InlineData(414, 441)]
+        [InlineData(144, 414)]
+        [InlineData(543, -1)]
+        [InlineData(987654321, -1)]
+        [InlineData(76281, 76812)]
+        [InlineData(564321, 612345)]
+        public void Test1(long n, long result)
+        {
+            Kata.NextBigger(n).Should().Be(result);
+        }
+    }
+}
